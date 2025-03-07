5aee6b2f6730091ea83feccaae8ecf41faf9f5ae
Lukas Yeung <44145664+LukasY98@users.noreply.github.com>
2025-03-07 16:15:36 +0100

    privacy spacing updated

--- PortfolioWebsite/privacy.php
+++ PortfolioWebsite/privacy.php
@@ -26,43 +26,43 @@
                     <p class="text-main txt-22">Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit
                         Ihren personenbezogenen Daten passiert, wenn Sie unsere Website besuchen. Personenbezogene Daten 
                         sind alle Daten, mit denen Sie persönlich identifiziert werden können.</p>
-                    <h4 class="mt-2 text-main">Datenerfassung auf unserer Website</h4>
+                    <h4 class="mt-5 text-main">Datenerfassung auf unserer Website</h4>
                     <h4 class="mt-2 text-main">Wer ist verantwortlich für die Datenerfassung auf dieser Website?</h4>
                     <p class="text-main txt-22">Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber:<br/>
                         Lukas Yeung<br/>Tannenweg 7b, 26340 Zetel<br/> lukasyeung@web.de</p>
-                    <h4 class="mt-2 text-main">Wie erfassen wir Ihre Daten?</h4>
+                    <h4 class="mt-5 text-main">Wie erfassen wir Ihre Daten?</h4>
                     <p class="text-main txt-22">Ihre Daten werden ausschließlich dadurch erhoben, dass Sie uns diese mitteilen,
                         z.B. durch das Ausfüllen des Kontaktformulars.</p>
-                    <h4 class="mt-2 text-main">Wofür nutzen wir Ihre Daten?</h4>
+                    <h4 class="mt-5 text-main">Wofür nutzen wir Ihre Daten?</h4>
                     <p class="text-main txt-22">Daten, die Sie im Kontaktformular eingeben, verwenden wir ausschließlich 
                         zur Beantwortung Ihrer Anfrage.</p>
 
-                    <h3 class="mt-4 text-main">2. Allgemeine Hinweise und Pflichtinformationen</h3>
+                    <h3 class="mt-6 text-main">2. Allgemeine Hinweise und Pflichtinformationen</h3>
                     <h4 class="mt-2 text-main">Datenschutz</h4>
                     <p class="text-main txt-22">Wir nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Ihre personenbezogenen 
                         Daten werden vertraulich und entsprechend den gesetzlichen Datenschutzvorschriften sowie dieser 
                         Datenschutzerklärung behandelt.</p>
-                    <h4 class="mt-2 text-main">Widerruf Ihrer Einwilligung zur Datenverarbeitung</h4>
+                    <h4 class="mt-5 text-main">Widerruf Ihrer Einwilligung zur Datenverarbeitung</h4>
                     <p class="text-main txt-22">Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung 
                         möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Eine formlose Mitteilung 
                         per E-Mail reicht dafür aus.</p>
-                    <h4 class="mt-2 text-main">Recht auf Auskunft, Berichtigung, Löschung</h4>
+                    <h4 class="mt-5 text-main">Recht auf Auskunft, Berichtigung, Löschung</h4>
                     <p class="text-main txt-22">Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit das 
                         Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft 
                         und Empfänger und den Zweck der Datenverarbeitung. Sie haben außerdem ein Recht auf Berichtigung 
                         oder Löschung dieser Daten.</p>
 
-                    <h3 class="mt-4 text-main">3. Datenerfassung auf dieser Website</h3>
+                    <h3 class="mt-6 text-main">3. Datenerfassung auf dieser Website</h3>
                     <h4 class="mt-2 text-main">Kontaktformular</h4>
                     <p class="text-main txt-22">Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre Angaben 
                         aus dem Formular inklusive der von Ihnen dort angegebenen Kontaktdaten zur Bearbeitung der Anfrage 
                         gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.</p>
-                    <h4 class="mt-2 text-main">Speicherdauer der Daten</h4>
+                    <h4 class="mt-5 text-main">Speicherdauer der Daten</h4>
                     <p class="text-main txt-22">Die von Ihnen im Kontaktformular eingegebenen Daten verbleiben bei uns, bis 
                         Sie uns zur Löschung auffordern, Ihre Einwilligung zur Speicherung widerrufen oder der Zweck für die 
                         Datenspeicherung entfällt (z.B. nach abgeschlossener Bearbeitung Ihrer Anfrage).</p>
 
-                    <h3 class="mt-4 text-main">4. Änderungen dieser Datenschutzerklärung</h3>
+                    <h3 class="mt-6 text-main">4. Änderungen dieser Datenschutzerklärung</h3>
                     <p class="text-main txt-22">Wir behalten uns vor, diese Datenschutzerklärung gelegentlich anzupassen, damit 
                         sie stets den aktuellen rechtlichen Anforderungen entspricht oder um Änderungen unserer Leistungen in der 
                         Datenschutzerklärung umzusetzen. Für Ihren erneuten Besuch gilt dann die neue Datenschutzerklärung.</p>
