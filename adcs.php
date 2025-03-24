721d035562badb360ab7dad84551ec03b1d31618
LukasY98 <lukasyeung@web.de>
2025-03-24 17:29:13 +0100

    added page for adcs project, adjusted column size of webscraper.php for better layout on smaller devices

--- adcs.php
+++ adcs.php
@@ -21,6 +21,91 @@
                 <div class="container">
                     <h1 class="mt-4 display-3 text-main text-center">Attitude Determination & Control System</h1>
                     <hr class="text-white">
+                    <div class="transparent-background mt-4">
+                        <h2 class="mt-3 display-4 text-main text-center">Aufbau</h2>
+                        <div class="row m-4 align-items-center">
+                            <div class="col-xl-4">
+                                <p class="text-main txt-22 text-center">Der Aufbau besteht aus einer Basisplattform, auf der die 
+                                    Hauptkomponenten wie Arduino, Breadboard, Motortreiber, Stromversorgung, Motor und LDRs befestigt sind.
+                                    Diese Plattform sitzt auf einem Lager, sodass sie sich drehen kann. Am Motorschaft ist das Reaction
+                                    Wheel montiert, das durch gezielte Drehbewegungen die Plattform stabilisiert und ausrichtet.</p>
+                            </div>
+                            <div class="col-xl-8">
+                                <div class="text-center">
+                                    <img src="resources/images/adcs_project_setup.png" class="img_project_small">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-6 m-4 align-items-center">
+                            <div class="col-xl-4">
+                                <p class="text-main txt-22 text-center">Die sechs LDRs sind gleichmäßig im Kreis auf der Basisplattform verteilt
+                                    und nach außen ausgerichtet. Diese Anordnung ermöglicht es, die hellste Lichtquelle in der Umgebung präzise 
+                                    zu erkennen, indem die Lichtintensitäten an verschiedenen Positionen verglichen werden.</p>
+                            </div>
+                            <div class="col-xl-8">
+                                <div class="text-center">
+                                    <img src="resources/images/adcs_project_setup_LDR.png" class="img_project_small">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="transparent-background mt-4">
+                        <h2 class="mt-3 display-4 text-main text-center">Reaction Wheel</h2>
+                        <div class="row m-4 align-items-center">
+                            <div class="col-xl-5">
+                                <p class="text-main txt-22 text-center">Das Reaction Wheel dient zur präzisen Ausrichtung der Plattform, indem es das 
+                                    physikalische Prinzip der Drehimpulserhaltung nutzt. Es ist direkt am Motor befestigt und erzeugt durch Beschleunigung
+                                    oder Abbremsen ein entgegengesetztes Drehmoment, das die Plattform in die gewünschte Richtung dreht. Dadurch kann die 
+                                    Ausrichtung zur Sonne ohne zusätzliche mechanische Aktoren erfolgen. In Kombination mit dem PID-Algorithmus ermöglicht 
+                                    es eine stabile und genaue Nachführung, ähnlich wie in der Satellitentechnik zur Lageregelung.</p>
+                            </div>
+                            <div class="col-xl-7">
+                                <div class="ratio ratio-16x9">
+                                    <div class="text-center">
+                                        <video src="resources/videos/demo.mp4" class="m-2 img_project_small w-100" controls></video>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="transparent-background mt-4">
+                        <h2 class="mt-3 display-4 text-main text-center">Code</h2>
+                        <div class="row m-4 align-items-center">
+                            <div class="col-xl-5">
+                                <p class="text-main txt-22 text-center">Der Error wird berechnet, indem zunächst der LDR mit dem höchsten Wert ermittelt wird,
+                                    um die grobe Sonnenrichtung zu bestimmen. Danach wird die Differenz der benachbarten LDRs analysiert, um das genaue Offset
+                                    zu berechnen. Je weiter die Sonne vom Frontsensor entfernt ist, desto größer ist der Error-Wert.</p>
+                            </div>
+                            <div class="col-xl-7">
+                                <div class="text-center">
+                                    <img src="resources/images/adcs_project_3.PNG" class="img_project_small">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row m-4 mt-6 align-items-center">
+                            <div class="col-xl-5">
+                                <p class="text-main txt-22 text-center">Das Integral im PID-Algorithmus summiert den Fehler über die Zeit, um langfristige Fehler zu
+                                    korrigieren. Bei Vorzeichenänderung oder Nullwert wird es zurückgesetzt. Es hilft, systematische Fehler zu beheben, die mit der
+                                    Proportionalsteuerung allein nicht korrigiert werden.</p>
+                            </div>
+                            <div class="col-xl-7">
+                                <div class="text-center">
+                                    <img src="resources/images/adcs_project_4.PNG" class="img_project_small">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row m-4 mt-6 align-items-center">
+                            <div class="col-xl-5">
+                                <p class="text-main txt-22 text-center">Der Derivativwert im PID-Algorithmus berechnet die Änderungsrate des Fehlers über die Zeit. 
+                                    Dadurch wird die Geschwindigkeit der Fehleränderung ermittelt, um schnelle Änderungen zu dämpfen und die Steuerung zu stabilisieren.</p>
+                            </div>
+                            <div class="col-xl-7">
+                                <div class="text-center">
+                                    <img src="resources/images/adcs_project_5.PNG" class="img_project_small">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <h2 class="mt-5 display-4 text-main text-center"><i class="fa-solid fa-person-digging"></i>Work in progress</h2>
                     <div class="mt-3 text-center">
                         <a href=portfolio.php class="btn btn-contact text-main txt-22"><i class="fa-solid fa-arrow-left"></i> Zurück</a>
--- webscraper.php
+++ webscraper.php
@@ -22,15 +22,15 @@
                     <h1 class="mt-4 display-3 text-main text-center">Webscraper</h1>
                     <hr class="text-white">
                     <div class="transparent-background mt-4">
-                        <h2 class="display-4 text-main text-center">Übesicht</h2>
+                        <h2 class="mt-3 display-4 text-main text-center">Übersicht</h2>
                         <div class="row m-4 align-items-center">
-                            <div class="col-lg-4">
+                            <div class="col-xl-4">
                                 <p class="text-main txt-22 text-center">Ein Python-basierter Web-Scraper, der Informationen der 
                                     Website <a href=https://books.toscrape.com class="text-main" target="_blank">Books to Scrape
                                     <i class="fa-solid fa-arrow-up-right-from-square small"></i></a> sammelt, kategorisch 
                                     zusammenfasst und analysiert.</p>
                             </div>
-                            <div class="col-lg-8">
+                            <div class="col-xl-8">
                                 <div class="text-center">
                                     <img src="resources/images/project_1.PNG" class="img_project_large">
                                 </div>
@@ -38,62 +38,62 @@
                         </div>
                     </div>
                     <div class="transparent-background mt-4">
-                        <h2 class="display-4 text-main text-center">Implementierung</h2>
+                        <h2 class="mt-3 display-4 text-main text-center">Implementierung</h2>
                         <div class="row m-4 mt-6 align-items-center">
-                            <div class="col-md-4">
+                            <div class="col-xl-4">
                                 <p class="text-main txt-22 text-center">Die Libraries "BeautifulSoup" und "Requests" werden verwendet,
                                     um die relevanten Websites zu fetchen.</p>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col-xl-8">
                                 <div class="text-center">
                                     <img src="resources/images/webscrape_project_1.PNG" class="img_project_small">
                                 </div>
                             </div>
                         </div>
                         <div class="row m-4 mt-6 align-items-center">
-                            <div class="col-md-3">
+                            <div class="col-xl-3">
                                 <p class="text-main txt-22 text-center">Es wird ein Dictonary angelegt. Für jedes Genre, für das es auf der 
                                     Website <span class="no_wrap"><a href=https://books.toscrape.com class="text-main" target="_blank">"Books to Scrape"
                                     <i class="fa-solid fa-arrow-up-right-from-square small"></i></a></span> Bücher gibt, wird ein
                                     Eintrag vorgenommen.</p>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-xl-9">
                                 <div class="text-center">
                                     <img src="resources/images/webscrape_project_2.PNG" class="img_project_medium">
                                 </div>
                             </div>
                         </div>
                         <div class="row m-4 mt-6 align-items-center">
-                            <div class="col-md-4">
+                            <div class="col-xl-4">
                                 <p class="text-main txt-22 text-center">Für jede Kategorie werden Informationen zu der Anzahl,
                                     der Bewertung und des Preises der Bücher gesammelt und in einer Tabelle zusammengefasst.
                                     (Für detaillierten Code, siehe <span class="no_wrap">
                                     <a href=https://github.com/LukasY98/Web-Scraper class="text-main" target="_blank">
                                     Github <i class="fa-solid fa-arrow-up-right-from-square small"></i></a></span>)</p>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col-xl-8">
                                 <div class="text-center">
                                     <img src="resources/images/webscrape_project_3.PNG" class="img_project_medium">
                                 </div>
                             </div>
                         </div>
                         <div class="row m-4 mt-6 align-items-center">
-                            <div class="col-md-4">
+                            <div class="col-xl-4">
                                 <p class="text-main txt-22 text-center">Die Daten können nach Bedarf in einer Datei (CSV, TXT oder JSON)
                                     gespeichert werden.</p>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col-xl-8">
                                 <div class="text-center">
                                     <img src="resources/images/webscrape_project_4.PNG" class="img_project_medium">
                                 </div>
                             </div>
                         </div>
                         <div class="row m-4 mt-6 align-items-center">
-                            <div class="col-md-3">
+                            <div class="col-xl-3">
                                 <p class="text-main txt-22 text-center">Optional können Grafiken erstellten werden, welche
                                     verschiede Fragen bezüglich der Daten beantworten.</p>
                             </div>
-                            <div class="col-md-9">
+                            <div class="col-xl-9">
                                 <div class="text-center">
                                     <img src="resources/images/webscrape_project_5.PNG" class="img_project_medium">
                                 </div>
